e3306f1d2001726f96fd651e971d4d662778d048
Craig Banton <c.banton12@gmail.com>
2015-05-21 17:11:16 -0400

    java fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root 'pages#home'
+
+  devise_for :users do
+  get 'logout' => 'devise/sessions#destroy'
+end
+
+  
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
